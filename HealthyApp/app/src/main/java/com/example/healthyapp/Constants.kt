e2dd407e06d5c4e25a04b5149cb925acda21d45a
AprioteseAndreea <andreea.apriotese11@gmail.com>
2021-05-18 21:38:25 +0300

    Added Constants & VolleyConfigSingleton classes

---
+++ HealthyApp/app/src/main/java/com/example/healthyapp/Constants.kt
@@ -0,0 +1,15 @@
+package com.example.healthyapp
+
+class Constants {
+    companion object {
+        const val MEALS_URL = "https://my-json-server.typicode.com/AprioteseAndreea/mealsJSON/meals"
+        const val ID = "id"
+        const val DAY= "day"
+        const val NAME = "name"
+        const val IMAGE_PATH = "imagePath"
+        const val INGREDIENTS = "ingredients"
+        const val HOW_TO_PREPARE = "howtoprepare"
+        const val PREP_TIME = "preptime"
+        const val CALORIES= "calories"
+    }
+}
