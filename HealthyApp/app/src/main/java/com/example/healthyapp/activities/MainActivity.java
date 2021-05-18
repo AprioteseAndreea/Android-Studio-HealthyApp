1628ee9253b4e4ab2994caa03b90c2322cd3fa03
AprioteseAndreea <andreea.apriotese11@gmail.com>
2021-05-18 21:34:06 +0300

    Added activities package

---
+++ HealthyApp/app/src/main/java/com/example/healthyapp/activities/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.healthyapp.activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.healthyapp.R;
+import com.example.healthyapp.activities.LoginActivity;
+import com.example.healthyapp.fragments.HomeFragment;
+import com.example.healthyapp.fragments.MealsFragment;
+import com.example.healthyapp.fragments.WorkoutFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /*Button logout = findViewById(R.id.logoutBtn);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                finish();
+            }
+        });*/
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                HomeFragment.newInstance(" ", " ")).commit();
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+                    switch(item.getItemId()){
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_meals:
+                            selectedFragment = new MealsFragment();
+                            break;
+                        case R.id.nav_workout:
+                            selectedFragment = new WorkoutFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
+}
