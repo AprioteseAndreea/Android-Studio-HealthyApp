8537ce648036836e617fb6270831ab59ce4caf49
AprioteseAndreea <andreea.apriotese11@gmail.com>
2021-05-18 21:36:42 +0300

    Added interfaces package

---
+++ HealthyApp/app/src/main/java/com/example/healthyapp/interfaces/ActivityFragmentCommunication.java
@@ -0,0 +1,6 @@
+package com.example.healthyapp.interfaces;
+
+public interface ActivityFragmentCommunication {
+    void replaceWithF2();
+
+}
---
+++ HealthyApp/app/src/main/java/com/example/healthyapp/interfaces/OnMealItemClick.java
@@ -0,0 +1,7 @@
+package com.example.healthyapp.interfaces;
+
+import com.example.healthyapp.models.Meal;
+
+public interface OnMealItemClick {
+    void onClick(Meal meal);
+}
