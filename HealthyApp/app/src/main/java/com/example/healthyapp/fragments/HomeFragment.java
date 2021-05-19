030d0ea6ee984765cf05ded462e72ae0567e74d1
AprioteseAndreea <andreea.apriotese11@gmail.com>
2021-05-19 23:21:09 +0300

    Updated some fragments, added some colors and added some dependencies in gradle

--- HealthyApp/app/src/main/java/com/example/healthyapp/fragments/HomeFragment.java
+++ HealthyApp/app/src/main/java/com/example/healthyapp/fragments/HomeFragment.java
@@ -1,14 +1,50 @@
 package com.example.healthyapp.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.healthyapp.R;
+import com.example.healthyapp.adapters.MealAdapter;
+import com.example.healthyapp.adapters.TodayMealsAdapter;
+import com.example.healthyapp.interfaces.ActivityFragmentCommunication;
+import com.example.healthyapp.interfaces.OnMealItemClick;
+import com.example.healthyapp.models.Meal;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import static com.example.healthyapp.Constants.CALORIES;
+import static com.example.healthyapp.Constants.DAY;
+import static com.example.healthyapp.Constants.HOW_TO_PREPARE;
+import static com.example.healthyapp.Constants.ID;
+import static com.example.healthyapp.Constants.IMAGE_PATH;
+import static com.example.healthyapp.Constants.INGREDIENTS;
+import static com.example.healthyapp.Constants.MEALS_URL;
+import static com.example.healthyapp.Constants.NAME;
+import static com.example.healthyapp.Constants.PREP_TIME;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,6 +61,11 @@ public class HomeFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private RecyclerView recyclerView;
+    ArrayList<Meal> meals = new ArrayList<>();
+    private ActivityFragmentCommunication activityFragmentCommunication;
+    private TextView dateTimeDisplay;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -60,7 +101,86 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        recyclerView = view.findViewById(R.id.today_meals_recyclerView);
+        dateTimeDisplay=view.findViewById(R.id.date_time);
+        getDate();
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext(), LinearLayoutManager.HORIZONTAL, false));
+
+        getMeals();
+        return view;
+    }
+    private void getDate(){
+
+
+        Calendar calendar;
+        SimpleDateFormat dateFormat;
+        String date;
+
+        calendar = Calendar.getInstance();
+
+        dateFormat = new SimpleDateFormat("EEE, MMM d, ''yy");
+        date = dateFormat.format(calendar.getTime());
+        dateTimeDisplay.setText(date);
+    }
+    private void getMeals() {
+        RequestQueue queue = Volley.newRequestQueue(getContext());
+        String url = MEALS_URL;
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+                            handleMealResponse(response);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getContext(), "ERROR: get EMPLOYEES failed with error: " + error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        queue.add(stringRequest);
+    }
+
+    private void handleMealResponse(String responseJson) throws JSONException {
+
+        JSONArray usersJSONArray = new JSONArray(responseJson);
+        for (int i = 0; i < usersJSONArray.length(); i++) {
+            JSONObject obj = usersJSONArray.getJSONObject(i);
+
+            String id = obj.getString(ID);
+            String day = obj.getString(DAY);
+            String name = obj.getString(NAME);
+            String imagePath = obj.getString(IMAGE_PATH);
+            String ingredients = obj.getString(INGREDIENTS);
+            String howtoprepare = obj.getString(HOW_TO_PREPARE);
+            String preptime = obj.getString(PREP_TIME);
+            String calories = obj.getString(CALORIES);
+
+            if(day.equals("1")){
+                Meal meal = new Meal(id, day,  name, preptime, calories, imagePath, ingredients, howtoprepare);
+                meals.add(meal);
+            }
+
+        }
+
+
+        TodayMealsAdapter adapter = new TodayMealsAdapter(meals);
+        recyclerView.setAdapter(adapter);
+
+    }
+    @Override
+    public void onAttach(@NonNull Context context) {
+
+        super.onAttach(context);
+        if (context instanceof ActivityFragmentCommunication) {
+            activityFragmentCommunication = (ActivityFragmentCommunication) context;
+        }
     }
 }
